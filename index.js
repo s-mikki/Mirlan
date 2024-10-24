167eee53a7d06805498e6523b802d3f91299791c
Mirlan Almazovich <strong.mirlan99@gmail.com>
2024-10-24 14:57:21 +0600

    Lesson 1

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ function division2 () {
     const input1 = parseFloat(document.getElementById('division2Inp').value);
     const input2 = parseInt(document.getElementById('division2Inp2').value);
     let sum = (input1 / input2) * 100 ;
-    document.getElementById('divisionResult2').innerHTML = sum
+    document.getElementById('divisionResult2').innerHTML = sum + "%"
 }
 
 function minus () {
-    const input1 = parseFloat(document.getElementById('inp1').value);
-    const input2 = parseInt(document.getElementById('inp2').value);
+    const input1 = parseFloat(document.getElementById('minusInp').value);
+    const input2 = parseInt(document.getElementById('minusInp2').value);
     let sum2 = input1 - input2;
-    document.getElementById('p1').innerHTML = sum2
+    document.getElementById('minusResult').innerHTML = sum2
 }
 
 function multiplication () {
-    const input1 = parseFloat(document.getElementById('inp1').value);
-    const input2 = parseInt(document.getElementById('inp2').value);
+    const input1 = parseFloat(document.getElementById('multiplicationInp').value);
+    const input2 = parseInt(document.getElementById('multiplicationInp2').value);
     let sum3 = input1 * input2;
-    document.getElementById('p1').innerHTML = sum3
+    document.getElementById('multiplicationResult').innerHTML = sum3
 }
 function clickButton () {
     const box = `<div class="box">footer</div>`
