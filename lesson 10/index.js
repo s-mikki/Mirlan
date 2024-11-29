9e65fc6052e2762fd486c1a80d774080108f6a46
Mirlan Almazovich <122715012+s-mikki@users.noreply.github.com>
2024-11-29 13:18:06 +0600

    Урок 9 домашняя

--- lesson 10/index.js
+++ lesson 10/index.js
@@ -60,3 +60,78 @@ function selectCountry () {
         document.getElementById('result').innerHTML = '';
     }
 }
+
+let categoryBox = [
+    {
+        id:1,
+        name:'Germany',
+        code:'+49',
+        shortName:'DE',
+        category:'men',
+        photo:'./img/vote-bg-1.jpg'
+    },
+    {
+
+        id:2,
+        name:'Kyrgyzstan',
+        code:'+996',
+        shortName:'KG',
+        category:'men',
+        photo:'./img/vote-bg-2.jpg'
+    },
+    {
+        id:3,
+        name:'Kazakhstan',
+        code:'+7',
+        shortName:'KZ',
+        category:'men',
+        photo:'./img/vote-bg-3.jpg'
+    },
+    {
+        id:4,
+        name:'Russia',
+        code:'+7',
+        shortName:'RU',
+        category:'men',
+        photo:'./img/vote-bg-4.jpg'
+    },
+    {
+        id:5,
+        name:'Pakistan',
+        code:'+92',
+        shortName:'PK',
+        category:'men',
+        photo:'./img/vote-bg-5.jpg'
+    },
+]
+
+function categorySearch () {
+    let searchValue = document.getElementById('categoryInput').value;
+    let data = categoryBox.filter((i)=> i.name.toLowerCase().includes(searchValue.toLowerCase()));
+    if(data.length > 0){
+        let text = '';
+        for(let i = 0; i<data.length; i++){
+            text += `<option value="${data[i].category}">${data[i].name}</option>`;
+        }
+        document.getElementById('categorySelect').innerHTML = text;
+    }else{
+        document.getElementById('categorySelect').innerHTML = '';
+    }
+}
+
+function categoryCountry () {
+    let categoryValue = document.getElementById('categorySelect').value;
+    let data = categoryBox.filter((i)=> i.category === categoryValue);
+    if(data.length > 0) {
+        document.getElementById('categoryResult').innerHTML =
+        `
+        <p>${data[0].name}</p>
+        <p>${data[0].code}</p>
+        <p>${data[0].shortName}</p>
+        <p>${data[0].category}</p>
+        <img src="${data[0].photo}" width="100" alt="фото">
+        `;
+    }else{
+        document.getElementById('categoryResult').innerHTML = '';
+    }
+}
