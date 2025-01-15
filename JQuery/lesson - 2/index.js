af880c6a09802fff6ebf47b3c582ffa2b18fea23
Mirlan Almazovich <strong.mirlan99@gmail.com>
2025-01-15 19:50:13 +0600

    jquery lesson 2

--- JQuery/lesson - 2/index.js
+++ JQuery/lesson - 2/index.js
@@ -1,44 +1,64 @@
-function time() {
-    $('.line__img').animate({
-        left: '615px',
-        transition: '4s'
-    })
-}
-
-setTimeout(time, 1000)
-
-
-function trafficGreen() {
+function trafficLights() {
+  function TGreen() {
     $('.line__circle--green').css({
-        backgroundColor: 'green',
-        transition: '2s'
-    })
-}
-
-setTimeout(trafficGreen, 2000)
-
-function trafficYellow() {
+      backgroundColor: 'green',
+      transition: '2s'
+    });
+    $('.line__circle--red, .line__circle--yellow').css({
+      backgroundColor: 'transparent',
+      transition: '2s'
+    });
+    startCar();
+  }
+  
+  function TYellow() {
     $('.line__circle--yellow').css({
-        backgroundColor: 'yellow',
-        transition: '2s'
+      backgroundColor: 'yellow',
+      transition: '2s'
     })
-    $('.line__circle--green').css({
-        backgroundColor: 'transparent',
-        transition: '2s'
+    $('.line__circle--green, .line__circle--red').css({
+      backgroundColor: 'transparent',
+      transition: '2s'
     })
-}
+    stopCar()
+  }
 
-setTimeout(trafficYellow, 4000)
-
-function trafficRed() {
+  function TRed() {
     $('.line__circle--red').css({
-        backgroundColor: 'red',
-        transition: '2s'
+      backgroundColor: 'red',
+      transition: '2s'
     })
-    $('.line__circle--yellow').css({
-        backgroundColor: 'transparent',
-        transition: '2s'
+    $('.line__circle--green, .line__circle--yellow').css({
+      backgroundColor: 'transparent',
+      transition: '2s'
     })
+    stopCar()
+  }
+
+  let state = 0
+
+  setInterval(() => {
+    if (state === 0) {
+      TGreen()
+    } else if (state === 1) {
+      TYellow()
+    }else if (state === 2) {
+      TRed()
+    }
+    state = (state + 1) % 3
+  }, 4000)
 }
 
-setTimeout(trafficRed, 6000)
+function startCar() {
+  let car =$('.line__img')
+  let carLeft = car.position().left
+  car.animate({
+    left: carLeft + 615 + 'px'
+  }, 4000)
+}
+
+function stopCar() {
+  $('.line__img').stop()
+}
+
+trafficLights()
